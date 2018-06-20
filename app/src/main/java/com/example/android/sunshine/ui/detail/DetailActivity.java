9aa5c010e677e8f72e49340888ebe955ed18cee1
Aleksandar Bozic <aleksandar.bozic@appculture.com>
2018-06-20 10:27:55 +0200

    Implemented steps 9.

--- app/src/main/java/com/example/android/sunshine/ui/detail/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/ui/detail/DetailActivity.java
@@ -21,6 +21,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.android.sunshine.AppExecutors;
 import com.example.android.sunshine.R;
 import com.example.android.sunshine.data.database.WeatherEntry;
 import com.example.android.sunshine.databinding.ActivityDetailBinding;
@@ -56,6 +57,31 @@ public class DetailActivity extends AppCompatActivity implements LifecycleOwner 
         Date date = new Date(timestamp);
 
         mViewModel = ViewModelProviders.of(this).get(DetailActivityViewModel.class);
+        mViewModel.getWeather().observe(this, weatherEntry -> {
+            if (weatherEntry != null)
+                bindWeatherToUI(weatherEntry);
+        });
+        loadDataFromDisk();
+    }
+
+    private void loadDataFromDisk() {
+        AppExecutors.getInstance().diskIO().execute(() -> {
+            try {
+
+                // Pretend this is the network loading data
+                Thread.sleep(4000);
+                Date today = SunshineDateUtils.getNormalizedUtcDateForToday();
+                WeatherEntry pretendWeatherFromDatabase = new WeatherEntry(1, 210, today, 88.0, 99.0, 71, 1030, 74, 5);
+                mViewModel.setWeather(pretendWeatherFromDatabase);
+
+                Thread.sleep(2000);
+                pretendWeatherFromDatabase = new WeatherEntry(1, 952, today, 50.0, 60.0, 46, 1044, 70, 100);
+                mViewModel.setWeather(pretendWeatherFromDatabase);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     private void bindWeatherToUI(WeatherEntry weatherEntry) {
